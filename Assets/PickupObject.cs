6e3cf1eae5a22652e0bef720b1d3f9da58c0774d
Francis <000766490@mohawkcollege.ca>
2017-11-30 18:57:28 -0500

    Intermittent Commit adding soundfx to pickups

--- Assets/PickupObject.cs
+++ Assets/PickupObject.cs
@@ -2,11 +2,24 @@
 using System.Collections;
 
 public class PickupObject : MonoBehaviour {
+	private bool triggered;
+	private AudioSource picUpSfx;
 
+	void Awake(){
+		picUpSfx = gameObject.GetComponent<AudioSource> ();
+	}
+	void Update(){
+		if(triggered && !picUpSfx.isPlaying)
+			Destroy (this.gameObject);
+	}
 	public void OnTriggerEnter2D(Collider2D collide){
 		Debug.Log ("Destroy the game object");
 		GameObject.FindGameObjectWithTag ("ScoreText").GetComponent<Score> ().IncreasScore (1);
-		Destroy (this.gameObject);
-		
+		//Destroy (this.gameObject);
+		//picUpSfx = this.gameObject.GetComponentInChildren(typeof(AudioSource)) as AudioSource;
+		//picUpSfx.Play ();
+		triggered = true;
+		picUpSfx.enabled = true;
+		picUpSfx.Play ();
 	}
 }
