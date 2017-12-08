254f678c049b17ac307b37e4f018d22ca45918c7
Francis <000766490@mohawkcollege.ca>
2017-12-07 20:03:56 -0500

    Class 2 Worked on User Interface,  There is a cool golden Bulet form Mario Brothers

---
+++ Assets/UI Class Stuff/MusicControl.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicControl : MonoBehaviour {
+
+	public void ChangeVolume(float newVolume)
+	{
+		Debug.Log ("Volume: "+newVolume);
+	}
+}
